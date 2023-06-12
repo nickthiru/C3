f36eaad44baa1b05a3ae77a1527164d57123909b
Nick Thiru <nickthiru@gmail.com>
2023-06-12 13:33:46 +0800

    Added folder to store all the classes and business logic i.e. services.
    Added a placeholder file to show the structure.

---
+++ packages/api/user-management/lib/services/AuthService.js
@@ -0,0 +1,8 @@
+// const lambda = require("aws-cdk-lib/aws-lambda");
+
+// const fn = new lambda.Function(this, "helloLambda", {
+//   runtime: lambda.Runtime.NODEJS_18_X,
+//   handler: "hello.handler",
+// });
+
+
