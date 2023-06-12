9872f660e8d02ce34843524a93c0bc8cf82cbedf
Nick Thiru <nickthiru@gmail.com>
2023-06-12 15:23:48 +0800

    Added a *Landing Page*.
    The Landing Page will be the first page a non-signed-in user will see when the visit the Home link.
    
    Commented out the code from the earlier "playground" and added *BrowserRouter* as well.

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,15 +1,32 @@
 // import WebSocketConnection from "./utils/WebSocketConnection";
-import Header from "./common/Header";
-import Navigation from "./common/Navigation";
-import Map from "./map/Map";
+// import Header from "./common/Header";
+// import Navigation from "./common/Navigation";
+// import Map from "./map/Map";
+// import "./App.css";
+
+// export default function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Navigation />
+//       <Map />
+//     </>
+//   );
+// }
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Navigation />
-      <Map />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
