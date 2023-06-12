3c6ca30bfb0c910ab54fc22f87f5fc9c9ece9b6e
Nick Thiru <nickthiru@gmail.com>
2023-06-12 19:04:38 +0800

    Added a basic, non-functional *Sign In* form on the Landing Page.

--- packages/web/src/pages/LandingPage.jsx
+++ packages/web/src/pages/LandingPage.jsx
@@ -1,5 +1,49 @@
+// function LandingPage() {
+//   return <div>Landing Page</div>;
+// }
+
+// export default LandingPage;
+
+import "./landing-page.css";
+
 function LandingPage() {
-  return <div>Landing Page</div>;
+  return (
+    <div
+      id="signin"
+      className="w3-container w3-padding-large w3-grey tab"
+      // style={{ display: "none" }}
+    >
+      <div className="w3-section">
+        <label>Username</label>
+        <input
+          className="w3-input w3-border"
+          type="text"
+          id="signin_username"
+          required
+        />
+      </div>
+      <div className="w3-section">
+        <label>Password</label>
+        <input
+          className="w3-input w3-border"
+          type="password"
+          id="signin_password"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w3-button w3-black w3-margin-bottom"
+        onClick={() => console.log("submit button clicked")}
+      >
+        Sign In
+      </button>
+      <div className="w3-section" id="tokens">
+        <pre className="" id="idToken"></pre>
+        <pre className="" id="accessToken"></pre>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
