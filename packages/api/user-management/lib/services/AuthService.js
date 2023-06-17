7e37b0c2779ae0e9123821b42cc2c95d1eb1a45b
Nick Thiru <nickthiru@gmail.com>
2023-06-17 16:21:22 +0800

    Decided on Factory Function implementation for services.
    Created a simple placeholder for now.

--- packages/api/user-management/lib/services/AuthService.js
+++ packages/api/user-management/lib/services/AuthService.js
@@ -1,8 +1,9 @@
-// const lambda = require("aws-cdk-lib/aws-lambda");
+function AuthService() {
+  // To do
 
-// const fn = new lambda.Function(this, "helloLambda", {
-//   runtime: lambda.Runtime.NODEJS_18_X,
-//   handler: "hello.handler",
-// });
+  return {
+    // API
+  }
+}
 
-
+module.exports = { AuthService }
