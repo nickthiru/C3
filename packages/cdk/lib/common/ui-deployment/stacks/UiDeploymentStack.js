3b8251c342ced1e4019ed2755181b823d5282bee
Nick Thiru <nickthiru@gmail.com>
2023-07-01 10:35:13 +0800

    Started working on UiDeploymentStack.

---
+++ packages/cdk/lib/common/ui-deployment/stacks/UiDeploymentStack.js
@@ -0,0 +1,14 @@
+const { Stack } = require("aws-cdk-lib");
+const { Bucket } = require("aws-cdk-lib/aws-s3");
+
+class UiDeploymentStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const deploymentBucket = new Bucket(this, "UiBucket", {
+      bucketName: "C3-frontend"
+    });
+  }
+}
+
+module.exports = { UiDeploymentStack };
