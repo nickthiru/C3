af47cb02ed56077c676d227788b323c37a8e60b0
Nick Thiru <nickthiru@gmail.com>
2023-07-04 20:24:21 +0800

    Started on Device Management

---
+++ packages/web/src/services/DeviceMgmtService.js
@@ -0,0 +1,16 @@
+// Provision device. Is it possible to use provisioning templates?
+
+  // Create an IoT thing
+
+  // Create X.509 cert
+
+  // Create IoT policy
+
+
+// Get devices detail + Search capability
+
+
+// Update device
+
+
+// Delete device
