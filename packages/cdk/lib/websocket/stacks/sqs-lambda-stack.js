e746f00d03665ca986aae144b9cc0b51d958f43d
Nick Thiru <nickthiru@gmail.com>
2023-07-07 09:52:12 +0800

    Refactored project directory and file names.

--- packages/cdk/lib/websocket/stacks/sqs-lambda-stack.js
+++ packages/cdk/lib/websocket/stacks/sqs-lambda-stack.js
@@ -13,7 +13,7 @@ class SqsLambdaStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    new SqsToLambda(this, 'AddUserSqsToLambda', {
+    new SqsToLambda(this, 'ToClientMessengerSqsToLambda', {
       lambdaFunctionProps: {
         runtime: Runtime.NODEJS_18_X,
         code: Code.fromAsset(path.join(__dirname, "../src/handlers")),
