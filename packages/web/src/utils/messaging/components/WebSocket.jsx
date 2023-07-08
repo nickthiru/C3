4608ec30c63a979d68bb8db4c68b919489a90aed
Nick Thiru <nickthiru@gmail.com>
2023-07-08 15:55:04 +0800

    Refactored WebSocket component to read the token value from secureLocalStorage.

--- packages/web/src/utils/messaging/components/WebSocket.jsx
+++ packages/web/src/utils/messaging/components/WebSocket.jsx
@@ -1,20 +1,12 @@
 import { useState, useEffect } from "react";
 import useWebSocket from "react-use-websocket";
-import { CdkStackWebSocketStackB6D3D0A7 as ws } from "../../../../../cdk/outputs.json";
+import { CdkStackWebSocketStackB6D3D0A7 as websocketStack } from "../../../../../cdk/outputs.json";
 import secureLocalStorage from "react-secure-storage";
 
-const WS_URL = `${ws.DevStageWebSocketApiEndpoint}`;
-const token = secureLocalStorage.getItem("token");
-
 export default function WebSocket() {
-  // const [token, setToken] = useState("");
+  const [token, setToken] = useState(() => secureLocalStorage.getItem("token"));
 
-  // useEffect(() => {
-  //   setToken(secureLocalStorage.getItem("token"));
-  //   console.log("token: " + token);
-  // }, [token]);
-
-  useWebSocket(WS_URL, {
+  useWebSocket(`${websocketStack.DevStageWebSocketApiEndpoint}`, {
     queryParams: { token: token },
     onOpen: (event) => console.log("(+) WebSocket connection opened"),
     onMessage: (message) => console.log(message),
